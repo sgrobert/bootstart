e80b8ec09cac62a170efae36e5102e995bd566b3
Robert Chai <sg.robert@gmail.com>
2018-09-18 18:49:06 +0800

    test push

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import Layout from '../components/layout'
 const IndexPage = () => (
   <Layout>
     <h1>Learn to <br />design and code React apps</h1>
-    <p>Complete courses about the best tools and design systems. Prototype and build apps with React and Swift.</p>
+    <p>Complete courses about the best tools and design systems. Prototype and build apps with React and Swift. I am happy with this.</p>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
